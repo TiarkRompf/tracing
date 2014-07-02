523931025829d69b7dd3d14a59983c055bea456e
Nada Amin <namin@alum.mit.edu>
2014-07-02 12:00:16 +0200

    oops, fix previous commit

--- src/test/scala/tracing.scala
+++ src/test/scala/tracing.scala
@@ -639,7 +639,7 @@ trait Analyze extends RunLowLevel {
         xs.groupBy(f1).map(kv => (kv._1,f2(kv._2)))
     }
 
-    val indexToBlock = t.distinct.toArray
+    val indexToBlock = traceB.distinct.toArray
     val blockToIndex = indexToBlock.zipWithIndex.toMap
     var trace = traceB map blockToIndex
     var interesting = if (tracePrefix != "") {
